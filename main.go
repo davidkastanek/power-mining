5cdf059617ca728786993548f3207c97debed09f
David Kastanek <david.kastanek@bloomreach.com>
2024-09-24 10:20:56 +0200

    Remove duplicit code

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/achetronic/tapogo/api/types"
 	"github.com/achetronic/tapogo/pkg/tapogo"
 	"gopkg.in/yaml.v3"
 	"log"
@@ -20,89 +21,67 @@ type Config struct {
 		Bucket string `yaml:"bucket"`
 	} `yaml:"influxdb"`
 	Tapo struct {
-		Email string `yaml:"email"`
-		Pass  string `yaml:"pass"`
-		Plugs []struct {
+		Email    string `yaml:"email"`
+		Password string `yaml:"password"`
+		Plugs    []struct {
 			Name string `yaml:"name"`
 			IP   string `yaml:"ip"`
 		} `yaml:"plugs"`
 	} `yaml:"tapo"`
 }
 
-func plugTurnOn(plug *tapogo.Tapo, ip string, email string, pass string, cooldown time.Duration) (*tapogo.Tapo, error) {
-	_, err := plug.TurnOn()
-	if err != nil {
-		for {
-			//fmt.Printf("Failed to turn on plug, retrying in %v ...\n", cooldown*time.Second)
-			time.Sleep(cooldown * time.Second)
-			newP, err := newPlug(ip, email, pass)
+type plugCredentials struct {
+	ip       string
+	email    string
+	password string
+}
+
+type action string
+
+func controlPlug(action action, credentials plugCredentials, cooldown time.Duration) (*types.ResponseSpec, error) {
+	for {
+		plug, err := tapogo.NewTapo(credentials.ip, credentials.email, credentials.password, &tapogo.TapoOptions{})
+		if err != nil {
+			return nil, fmt.Errorf("failed to connect to plug: %w", err)
+		}
+
+		switch action {
+		case "TurnOn":
+			response, err := plug.TurnOn()
 			if err != nil {
-				return nil, err
+				time.Sleep(cooldown * time.Second)
+			} else {
+				return response, nil
 			}
-			_, err = newP.TurnOn()
-			if err == nil {
-				//fmt.Println("Plug turned on")
-				return newP, nil
+		case "TurnOff":
+			response, err := plug.TurnOff()
+			if err != nil {
+				time.Sleep(cooldown * time.Second)
+			} else {
+				return response, nil
 			}
+		case "DeviceInfo":
+			response, err := plug.DeviceInfo()
+			if err != nil {
+				time.Sleep(cooldown * time.Second)
+			} else {
+				return response, nil
+			}
+		default:
+			return nil, fmt.Errorf("unknown action: %s", action)
 		}
 	}
-	return plug, nil
 }
 
-func plugIsOn(plug *tapogo.Tapo, ip string, email string, pass string, cooldown time.Duration) (bool, *tapogo.Tapo, error) {
-	info, err := plug.DeviceInfo()
-	if err != nil {
-		for {
-			//fmt.Printf("Failed to get plug state, retrying in %v ...\n", cooldown*time.Second)
-			time.Sleep(cooldown * time.Second)
-			newP, err := newPlug(ip, email, pass)
-			if err != nil {
-				return false, nil, err
-			}
-			info, err = newP.DeviceInfo()
-			if err == nil {
-				//fmt.Println("Plug state gathered")
-				return info.Result.DeviceOn, newP, nil
-			}
-		}
-	}
-	return info.Result.DeviceOn, plug, nil
+func plugIsOn(response *types.ResponseSpec) bool {
+	return response.Result.DeviceOn
 }
 
-func plugTurnOff(plug *tapogo.Tapo, ip string, email string, pass string, cooldown time.Duration) (*tapogo.Tapo, error) {
-	_, err := plug.TurnOff()
-	if err != nil {
-		for {
-			//fmt.Printf("Failed to turn off plug, retrying in %v ...\n", cooldown*time.Second)
-			time.Sleep(cooldown * time.Second)
-			newP, err := newPlug(ip, email, pass)
-			if err != nil {
-				return nil, err
-			}
-			_, err = newP.TurnOff()
-			if err == nil {
-				//fmt.Println("Plug turned off")
-				return newP, nil
-			}
-		}
-	}
-	return plug, nil
-}
-
-func newPlug(ip string, email string, password string) (*tapogo.Tapo, error) {
-	plug, err := tapogo.NewTapo(ip, email, password, &tapogo.TapoOptions{})
-	if err != nil {
-		return nil, fmt.Errorf("error connecting to plug: %v", err)
-	}
-
-	return plug, nil
-}
-
-func queryInfluxDB(client influxdb2.Client, org string, fluxQuery string) float64 {
+func queryInfluxDB(client influxdb2.Client, org string, fluxQuery string) (float64, error) {
 	queryAPI := client.QueryAPI(org)
 	result, err := queryAPI.Query(context.Background(), fluxQuery)
 	if err != nil {
-		log.Fatalf("Query failed: %v\n", err)
+		return 0, fmt.Errorf("error querying influxdb: %v", err)
 	}
 
 	var value float64
@@ -110,9 +89,9 @@ func queryInfluxDB(client influxdb2.Client, org string, fluxQuery string) float6
 		value = result.Record().Value().(float64)
 	}
 	if result.Err() != nil {
-		log.Fatalf("Query parsing failed: %v\n", result.Err().Error())
+		return 0, fmt.Errorf("error parsing influxdb query result: %v", result.Err())
 	}
-	return value
+	return value, nil
 }
 
 func main() {
@@ -133,7 +112,7 @@ func main() {
 	defer influxClient.Close()
 
 	for {
-		batterySoC := queryInfluxDB(influxClient, config.InfluxDB.Org, `
+		batterySoC, err := queryInfluxDB(influxClient, config.InfluxDB.Org, `
 			from(bucket: "homeassistant")
 			  |> range(start: -1h)
 			  |> filter(fn: (r) => r["entity_id"] == "battery_state_of_charge")
@@ -141,8 +120,11 @@ func main() {
 			  |> filter(fn: (r) => r["_measurement"] == "%")
 			  |> aggregateWindow(every: 1m, fn: mean, createEmpty: false)
 			  |> yield(name: "mean")`)
+		if err != nil {
+			log.Fatalf("Error querying InfluxDB: %v", err)
+		}
 
-		//loadL2 := queryInfluxDB(influxClient, config.InfluxDB.Org, `
+		//loadL2, err := queryInfluxDB(influxClient, config.InfluxDB.Org, `
 		//	from(bucket: "homeassistant")
 		//	  |> range(start: -1h)
 		//	  |> filter(fn: (r) => r["entity_id"] == "load_l2")
@@ -150,8 +132,11 @@ func main() {
 		//	  |> filter(fn: (r) => r["_measurement"] == "W")
 		//	  |> aggregateWindow(every: 1m, fn: mean, createEmpty: false)
 		//	  |> yield(name: "mean")`)
+		//if err != nil {
+		//	log.Fatalf("Error querying InfluxDB: %v", err)
+		//}
 
-		loadL3 := queryInfluxDB(influxClient, config.InfluxDB.Org, `
+		loadL3, err := queryInfluxDB(influxClient, config.InfluxDB.Org, `
 			from(bucket: "homeassistant")
 			  |> range(start: -1h)
 			  |> filter(fn: (r) => r["entity_id"] == "load_l3")
@@ -159,8 +144,11 @@ func main() {
 			  |> filter(fn: (r) => r["_measurement"] == "W")
 			  |> aggregateWindow(every: 1m, fn: mean, createEmpty: false)
 			  |> yield(name: "mean")`)
+		if err != nil {
+			log.Fatalf("Error querying InfluxDB: %v", err)
+		}
 
-		pvPower := queryInfluxDB(influxClient, config.InfluxDB.Org, `
+		pvPower, err := queryInfluxDB(influxClient, config.InfluxDB.Org, `
 			from(bucket: "homeassistant")
 			  |> range(start: -1h)
 			  |> filter(fn: (r) => r["entity_id"] == "pv_power")
@@ -168,8 +156,11 @@ func main() {
 			  |> filter(fn: (r) => r["_measurement"] == "W")
 			  |> aggregateWindow(every: 1m, fn: mean, createEmpty: false)
 			  |> yield(name: "mean")`)
+		if err != nil {
+			log.Fatalf("Error querying InfluxDB: %v", err)
+		}
 
-		tuvTemp := queryInfluxDB(influxClient, config.InfluxDB.Org, `
+		tuvTemp, err := queryInfluxDB(influxClient, config.InfluxDB.Org, `
 			from(bucket: "homeassistant")
 			  |> range(start: -1h)
 			  |> filter(fn: (r) => r["domain"] == "sensor")
@@ -178,56 +169,70 @@ func main() {
 			  |> filter(fn: (r) => r["_measurement"] == "°C")
 			  |> aggregateWindow(every: 1m, fn: mean, createEmpty: false)
 			  |> yield(name: "mean")`)
-
-		var tuvPlug *tapogo.Tapo
-		tuvPlug, err = tapogo.NewTapo(config.Tapo.Plugs[1].IP, config.Tapo.Email, config.Tapo.Pass, &tapogo.TapoOptions{})
 		if err != nil {
-			log.Fatal("Failed to connect to plug", err)
+			log.Fatalf("Error querying InfluxDB: %v", err)
 		}
-		var tuvPlugState bool
-		tuvPlugState, tuvPlug, err = plugIsOn(tuvPlug, config.Tapo.Plugs[1].IP, config.Tapo.Email, config.Tapo.Pass, 3)
+
+		var turnOnAction action = "TurnOn"
+		var turnOffAction action = "TurnOff"
+		var deviceInfoAction action = "DeviceInfo"
+		const COOLDOWN = 3
+
+		var tuvPlugCredentials = plugCredentials{
+			ip:       config.Tapo.Plugs[1].IP,
+			email:    config.Tapo.Email,
+			password: config.Tapo.Password,
+		}
+		tuvPlugInfo, err := controlPlug(deviceInfoAction, tuvPlugCredentials, COOLDOWN)
+		if err != nil {
+			log.Fatalf("Error getting device info: %v", err)
+		}
+		tuvPlugState := plugIsOn(tuvPlugInfo)
 
 		if (!tuvPlugState && (batterySoC == 0 || batterySoC == 100) && loadL3 < 1000 && tuvTemp < 67) ||
 			(tuvPlugState && (batterySoC == 0 || batterySoC == 100) && loadL3 < 3000 && pvPower > 2000 && tuvTemp < 67) ||
 			(!tuvPlugState && batterySoC > 40 && loadL3 < 1000 && pvPower > 2000 && tuvTemp < 67) ||
 			(tuvPlugState && batterySoC > 40 && loadL3 < 3000 && pvPower > 2000 && tuvTemp < 67) {
 			fmt.Printf("%v TUV: ON - PlugState: %v, SoC: %.1f, L3 Load: %.0f, Temp: %.1f, Panels: %.0f\n", time.Now().Format("2006-01-02 15:04:05"), tuvPlugState, batterySoC, loadL3, tuvTemp, pvPower)
-			tuvPlug, err = plugTurnOn(tuvPlug, config.Tapo.Plugs[1].IP, config.Tapo.Email, config.Tapo.Pass, 3)
+			_, err = controlPlug(turnOnAction, tuvPlugCredentials, COOLDOWN)
 			if err != nil {
-				log.Fatal("Failed to turn on tuv plug", err)
+				log.Fatalf("Error turning on plug: %v", err)
 			}
 		} else {
 			fmt.Printf("%v TUV: OFF - PlugState: %v, SoC: %.1f, L3 Load: %.0f, Temp: %.1f, Panels: %.0f\n", time.Now().Format("2006-01-02 15:04:05"), tuvPlugState, batterySoC, loadL3, tuvTemp, pvPower)
-			tuvPlug, err = plugTurnOff(tuvPlug, config.Tapo.Plugs[1].IP, config.Tapo.Email, config.Tapo.Pass, 3)
+			_, err = controlPlug(turnOffAction, tuvPlugCredentials, COOLDOWN)
 			if err != nil {
-				log.Fatal("Failed to turn off tuv plug", err)
+				log.Fatalf("Error turning off plug: %v", err)
 			}
 		}
 
 		time.Sleep(150 * time.Second)
 
-		//var heaterPlug *tapogo.Tapo
-		//heaterPlug, err = tapogo.NewTapo(config.Tapo.Plugs[0].IP, config.Tapo.Email, config.Tapo.Pass, &tapogo.TapoOptions{})
-		//if err != nil {
-		//	log.Fatal("Failed to connect to plug", err)
+		//var heaterPlugCredentials = plugCredentials{
+		//	ip:       config.Tapo.Plugs[0].IP,
+		//	email:    config.Tapo.Email,
+		//	password: config.Tapo.Password,
 		//}
-		//var heaterPlugState bool
-		//heaterPlugState, heaterPlug, err = plugIsOn(heaterPlug, config.Tapo.Plugs[0].IP, config.Tapo.Email, config.Tapo.Pass, 3)
-
+		//heaterPlugInfo, err := controlPlug(deviceInfoAction, heaterPlugCredentials, COOLDOWN)
+		//if err != nil {
+		//	log.Fatalf("Error getting device info: %v", err)
+		//}
+		//heaterPlugState := plugIsOn(heaterPlugInfo)
+		//
 		//if (!tuvPlugState && !heaterPlugState && (batterySoC == 0 || batterySoC == 100) && loadL2 < 1000) ||
 		//	(!tuvPlugState && heaterPlugState && (batterySoC == 0 || batterySoC == 100) && loadL2 < 3000 && pvPower > 2000) ||
 		//	(!tuvPlugState && !heaterPlugState && batterySoC > 60 && loadL2 < 1000 && pvPower > 2000) ||
 		//	(!tuvPlugState && heaterPlugState && batterySoC > 60 && loadL2 < 3000 && pvPower > 2000) {
 		//	fmt.Printf("%v HEATER: ON - PlugState: %v, SoC: %.1f, L2 Load: %.0f, Panels: %.0f\n", time.Now().Format("2006-01-02 15:04:05"), heaterPlugState, batterySoC, loadL2, pvPower)
-		//	heaterPlug, err = plugTurnOn(heaterPlug, config.Tapo.Plugs[0].IP, config.Tapo.Email, config.Tapo.Pass, 3)
+		//	_, err = controlPlug(turnOnAction, heaterPlugCredentials, COOLDOWN)
 		//	if err != nil {
-		//		log.Fatal("Failed to turn on heater plug", err)
+		//		log.Fatalf("Error turning on plug: %v", err)
 		//	}
 		//} else {
 		//	fmt.Printf("%v HEATER: OFF - PlugState: %v, SoC: %.1f, L2 Load: %.0f, Panels: %.0f\n", time.Now().Format("2006-01-02 15:04:05"), heaterPlugState, batterySoC, loadL2, pvPower)
-		//	heaterPlug, err = plugTurnOff(heaterPlug, config.Tapo.Plugs[0].IP, config.Tapo.Email, config.Tapo.Pass, 3)
+		//	_, err = controlPlug(turnOffAction, heaterPlugCredentials, COOLDOWN)
 		//	if err != nil {
-		//		log.Fatal("Failed to turn off heater plug", err)
+		//		log.Fatalf("Error turning off plug: %v", err)
 		//	}
 		//}
 
